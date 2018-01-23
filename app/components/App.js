1822e678dd6e8e55396f394799b53727aba09fef
Yungil Hong <hongyi0220@gmail.com>
2018-01-22 20:29:53 -0600

    Add stockchart, ability to add stock

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,158 @@
 import React from 'react';
 import { withRouter, Route, Switch } from 'react-router-dom';
+import socketIOClient from 'socket.io-client';
+import Highcharts from 'highcharts/highstock';
 
 class App extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            input: null,
+            dataset: [],
+            stockSymbols: []
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+        this.buildChart = this.buildChart.bind(this);
+    }
+
+    getData(stockSym) {
+        // console.log('stockSym arg @ getData:', stockSym);
+        const api_key = 'mx7b4emwTWnteEaLCztY';
+        const api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockSym + '/data.json?api_key=';
+
+        return fetch(api_url + api_key)
+        .then(res => res.json())
+        .then(resJson => {
+            // console.log('resJson:', resJson);
+            const state = {...this.state};
+            let dataset = state.dataset;
+
+            if (resJson['quandl_error']) {
+                throw new Error(resJson['quandl_error'].message);
+            }
+            const transformedData = transformData(resJson);
+
+            dataset.push(transformedData)
+            this.setState({state}/*, () => console.log('state after setState @ getData:', this.state)*/);
+            return transformedData;
+        })
+        .catch(err => {
+            console.log(err);
+            return false;
+        });
+
+        function transformData(data) {
+            data = data['dataset_data'].data.map(d => {
+                const dateString = d[0];
+                const date = Date.parse(dateString);
+                const stockValue = d[4];
+                return [date, stockValue];
+            });
+            // console.log('transformedData:', data);
+            return data.reverse();
+        }
+    }
+
+    handleInput(evt) {
+        const input = evt.target.value;
+        // console.log('input:', input);
+        this.setState({
+            input: input
+        });
+    }
+
+    handleSubmit() {
+        const socket = socketIOClient();
+        const state = {...this.state};
+        const input = state.input;
+        const symbol = input.trim().toUpperCase();
+        // console.log('trimmed input(symbol):', symbol);
+        const stockSymbols = state.stockSymbols;
+
+        function hasSymbol(sym) {
+            for (let i = 0; i < stockSymbols.length; i++) {
+                if (stockSymbols[i] === sym) return true;
+            }
+            return false;
+        }
+        this.getData(symbol)
+        .then(result => {
+            // console.log('result:', result);
+            // console.log('!hasSymbol:', !hasSymbol(symbol));
+            if (!hasSymbol(symbol) && result) stockSymbols.push(symbol);
+            this.setState({state}, () => {
+                socket.emit('stock symbols', this.state.stockSymbols);
+                socket.emit('stock dataset', this.state.dataset);
+                // console.log('state after setState:', this.state);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    }
+
+    buildChart(where, dataset, stockSymbols) {
+        let series = [];
+
+        for (let i = 0; i < stockSymbols.length; i++) {
+            const datum = {
+                name: stockSymbols[i],
+                data: dataset[i],
+                tooltip: {
+                    valueDecimals: 2
+                }
+            };
+            series.push(datum);
+        }
+
+        const highcharts = new Highcharts.stockChart(where, {
+            rangeSelector: {
+                selected: 1
+            },
+            title:  {
+                text: 'Stock'
+            },
+            series: series
+        });
+    }
+
+    componentDidMount() {
+        const socket = socketIOClient();
+
+        socket.on('stock symbols', symbols => this.setState({stockSymbols: symbols}));
+        socket.on('stock dataset', dataset => {
+
+            const stockSymbols = this.state.stockSymbols;
+            this.setState({dataset: dataset});
+            this.buildChart(chartContainer, dataset, stockSymbols);
+            // console.log('setState @ compDidMnt:', this.state);
+        });
+        const chartContainer = document.querySelector('.chart-container');
+        const state = {...this.state};
+        // console.log('{...this.state} @ compDidMnt:', state);
+        const stockSymbols = state.stockSymbols;
+        const dataset = state.dataset;
+
+        // this.buildChart(chartContainer, dataset, stockSymbols);
     }
 
     render() {
+        const handleSubmit = this.handleSubmit;
+        const handleInput = this.handleInput;
+        const stockSymbols = this.state.stockSymbols;
+
         return (
-            <div>Welcome to Stock Chart</div>
+            <div className='app-container'>
+                <div className='chart-container'></div>
+                <div className='cards-container'>Stock cards go here</div>
+                <div className='search-bar'>
+                    <input onChange={handleInput} type='text' placeholder='APPL'/>
+                    <button onClick={handleSubmit}>Add</button>
+                </div>
+            </div>
         );
     }
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
 const port = process.env.PORT || 8080;
+const io = require('socket.io')(http);
 
 app.use(express.static('build'));
 
@@ -8,4 +10,21 @@ app.get('*', (req, res) => {
     res.sendFile(__dirname + '/build/index.html')
 });
 
-app.listen(port);
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('stock symbols', function(symbols) {
+        console.log('symbols recieved @ server:', symbols);
+        socket.broadcast.emit('stock symbols', symbols);
+    });
+    socket.on('stock dataset', function(dataset) {
+        console.log('dataset recieved @ server:', dataset);
+        socket.broadcast.emit('stock dataset', dataset);
+    });
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(port, function() {
+    console.log('listening on: 8080');
+});
